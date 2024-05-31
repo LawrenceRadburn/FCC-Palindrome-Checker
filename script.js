18b308b60f4d585d0b25ed762563908a03869868
Lawrence Radburn <lawrence@radnet.ca>
2024-05-31 18:17:09 -0400

    feat: add palindrome to array

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-    const checkButton = document.getElementById("check-btn");
-    const textInput = document.getElementById("text-input");
+const checkButton = document.getElementById("check-btn");
+const textInput = document.getElementById("text-input");
     
-    checkButton.addEventListener("click", function emptyInputBox() {
-        if (textInput.value === "") {
-            alert("Please input a value");
+checkButton.addEventListener("click", function() {
+    const input = textInput.value.toLowerCase();
+    const palindromes = ["A", "eye"];
+        
+    if (input === "") {
+        alert("Please enter a value");
+        return;
+    }
+
+    let isPalindrome = false;
+    for (const word of palindromes) {
+        if (input === word.toLowerCase()) {
+            isPalindrome = true;
+            break;
         }
-    });
+    }
+    if (isPalindrome) {
+        alert(`${input} is a palindrome`);
+    }
+    else {
+        alert(`${input} is not a palindrome.`);
+    }
+});
